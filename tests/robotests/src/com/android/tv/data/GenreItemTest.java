620022a5bb9d6a8af513be01518bb7a8a3273ce1
Nick Chalko <nchalko@google.com>
2019-10-25 16:14:39 -0700

    Remove unused java imports
    PiperOrigin-RevId: 276780569
    Change-Id: I0b38bfce5ad9cb0d2a7bebebcebb24411b7c72c2

--- tests/robotests/src/com/android/tv/data/GenreItemTest.java
+++ tests/robotests/src/com/android/tv/data/GenreItemTest.java
@@ -26,7 +26,6 @@ import com.android.tv.testing.constants.ConfigConstants;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
