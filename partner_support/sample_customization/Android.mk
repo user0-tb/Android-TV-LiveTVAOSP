80fac3bfc7f6fc224ef63fdb73e00b4233db3458
Bob Badour <bbadour@google.com>
2021-04-20 14:07:16 -0700

    [LSC] Add LOCAL_LICENSE_KINDS to packages/apps/TV
    Added SPDX-license-identifier-Apache-2.0 to:
      partner_support/sample_customization/Android.mk
      tests/func/Android.mk
      tests/input/Android.mk
      tests/jank/Android.mk
      tests/unit/Android.mk
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    
    Exempt-From-Owner-Approval: janitorial work
    Change-Id: Id5a01145ce5bac20884ed42a103d4a02058fb5fd

--- partner_support/sample_customization/Android.mk
+++ partner_support/sample_customization/Android.mk
@@ -2,6 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_PACKAGE_NAME := PartnerSupportSampleCustomization
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
+LOCAL_LICENSE_CONDITIONS := notice
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_PROGUARD_ENABLED := disabled
